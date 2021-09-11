501a12471864e1fc0be05c9b1914754cf13a418c
Artour <benevolenskiartour@gmail.com>
2021-09-11 14:09:52 -0400

    Esquisse du issue #1

--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -4,11 +4,16 @@ import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 
+import { HighestScoresComponent } from '@app/pages/highest-scores/highest-scores.component';
+import { GameSelectionPageComponent } from '@app/pages/game-selection-page/game-selection-page.component';
+
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: MainPageComponent },
     { path: 'game', component: GamePageComponent },
     { path: 'material', component: MaterialPageComponent },
+    { path: 'scores', component: HighestScoresComponent },
+    { path: 'selection', component: GameSelectionPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
