3ab32de4074f0cb76aea06a26028450a2c1ebcc9
Anthony Hotte <anthonyhotte10@gmail.com>
2021-09-13 17:12:44 -0400

    A.H: Refactoring pour que le lint passe

--- client/src/app/pages/solo-game-initiator/solo-game-initiator.component.spec.ts
+++ client/src/app/pages/solo-game-initiator/solo-game-initiator.component.spec.ts
@@ -3,23 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SoloGameInitiatorComponent } from './solo-game-initiator.component';
 
 describe('SoloGameInitiatorComponent', () => {
-  let component: SoloGameInitiatorComponent;
-  let fixture: ComponentFixture<SoloGameInitiatorComponent>;
+    let component: SoloGameInitiatorComponent;
+    let fixture: ComponentFixture<SoloGameInitiatorComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SoloGameInitiatorComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [SoloGameInitiatorComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SoloGameInitiatorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SoloGameInitiatorComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
