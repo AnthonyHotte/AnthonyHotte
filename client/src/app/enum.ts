1c2c919612dc169428cb69dbb1683716ebee8ce6
Anthony Hotte <anthonyhotte10@gmail.com>
2021-09-13 14:43:45 -0400

    A.H: Ajout de Classe Grid et Tile et refactoring

--- client/src/app/enum.ts
+++ client/src/app/enum.ts
@@ -1,27 +1,7 @@
 export enum TileType {
-    Normal = 'normal',
-    DoubleWord = 'doubleWord',
-    TripleWord = 'tripleWord',
-    DoubleLetter = 'doubleLetter',
-    TripleLetter = 'tripleLetter',
+    Normal,
+    DoubleWord,
+    TripleWord,
+    DoubleLetter,
+    TripleLetter,
 }
-
-export const getEnumType = (stringType: string): TileType => {
-    switch (stringType) {
-        case 'doubleWord': {
-            return TileType.DoubleWord;
-        }
-        case 'doubleLetter': {
-            return TileType.DoubleLetter;
-        }
-        case 'tripleWord': {
-            return TileType.TripleWord;
-        }
-        case 'tripleLetter': {
-            return TileType.TripleLetter;
-        }
-        default: {
-            return TileType.Normal;
-        }
-    }
-};
--- client/src/app/tile.ts
+++ client/src/app/tile.ts
@@ -1,12 +1,20 @@
-import { TileType, getEnumType } from './enum';
+import { TileType } from './enum';
+import { isRedCase, isDarkBlueCase, isLightBlueCase, isPinkCase } from './tile-type-usefull-function';
 
 export class Tile {
     tileType: TileType;
-    constructor(public positionX: number, public positionY: number, tileTypeString: string, public isEmpty: boolean, public charOnIt: string) {
-        this.tileType = getEnumType(tileTypeString);
-    }
-
-    setTileType(tileTypeString: string) {
-        this.tileType = getEnumType(tileTypeString);
+    // Warning position of a Tile begins at one!
+    constructor(public positionX: number, public positionY: number, public isEmpty: boolean = false, public charOnIt: string = '') {
+        if (isRedCase(positionX, positionY)) {
+            this.tileType = TileType.TripleWord;
+        } else if (isPinkCase(positionX, positionY)) {
+            this.tileType = TileType.DoubleWord;
+        } else if (isDarkBlueCase(positionX, positionY)) {
+            this.tileType = TileType.TripleLetter;
+        } else if (isLightBlueCase(positionX, positionY)) {
+            this.tileType = TileType.DoubleLetter;
+        } else {
+            this.tileType = TileType.Normal;
+        }
     }
 }
