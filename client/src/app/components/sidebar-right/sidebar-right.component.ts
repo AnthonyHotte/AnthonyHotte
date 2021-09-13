460e252fc43b214ff7499ae3421f288b896c3475
Artour <benevolenskiartour@gmail.com>
2021-09-13 16:56:23 -0400

    Erreur de commit, ajout du reste

---
+++ client/src/app/components/sidebar-right/sidebar-right.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { SoloModeInformationsService } from '@app/services/solo-mode-informations.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-sidebar-right',
+  templateUrl: './sidebar-right.component.html',
+  styleUrls: ['./sidebar-right.component.scss']
+})
+
+export class SidebarRightComponent implements OnInit {
+
+    private inscription: Subscription;
+    nomJoueur: string = '';
+    nomAdversaire: string = '';
+    difficulteFacile: boolean;
+    temps: number;
+    message: string[]; 
+
+    constructor(private informationsJeuSolo: SoloModeInformationsService) {}
+
+    ngOnInit(): void {
+      this.inscription = this.informationsJeuSolo.messageCourant.subscribe(message => this.message = message);
+      this.setAttribut();
+    }
+
+    setAttribut(){
+      this.nomJoueur = this.message[0];
+      this.nomAdversaire = this.message[1];
+      this.difficulteFacile = this.message[2] === 'true';
+      this.temps = parseInt(this.message[3]);
+    }
+
+    ngOnDestroy(){
+      this.inscription.unsubscribe();
+    }
+}
