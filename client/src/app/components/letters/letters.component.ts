a8a7f93ee3820a095ba13875477ab855421c3985
Artour <benevolenskiartour@gmail.com>
2021-09-14 23:28:07 -0400

    Avancement sur la creation de l'ennemi et du joueur

--- client/src/app/components/letters/letters.component.ts
+++ client/src/app/components/letters/letters.component.ts
@@ -14,13 +14,13 @@ export class LettersComponent implements OnInit {
     letters: Letter[] = []; // the hand of the user
     buttonPressed: string = ''; // the key that is being pressed by the user.
     MAXLETTERSINHAND: number = this.letterService.MAXLETTERSINHAND; // constant that is supposed to be in the constant file
-    currentLetterNumber: number = this.letterService.currentLetterNumber;
+    currentLetterNumber: number = this.letterService.currentLetterNumberForPlayer;
 
     constructor(private letterService: LetterService) {}
 
     getNewLetters(amount: number): void {
         if(this.currentLetterNumber <= this.MAXLETTERSINHAND){
-            this.letterService.addLetters(amount);
+            this.letterService.addLettersForPlayer(amount);
             this.letters = this.letterService.getLetters();
             this.currentLetterNumber = parseInt(this.message);
         }
@@ -33,5 +33,9 @@ export class LettersComponent implements OnInit {
     ngOnInit(): void {
         this.getNewLetters(this.MAXLETTERSINHAND);
         this.subscription = this.letterService.currentMessage.subscribe(message => this.message = message);
+    }
+
+    getLetters(){
+        this.letterService.allLetters.length;
     }
 }
--- client/src/app/services/solo-player.service.ts
+++ client/src/app/services/solo-player.service.ts
@@ -1,22 +1,31 @@
+//https://fireship.io/lessons/sharing-data-between-angular-components-four-methods/
 import { Injectable } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { BehaviorSubject, Subscription } from 'rxjs';
 import { LetterService } from './letter.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class SoloPlayerService {
-    
     message: string;
     subscription: Subscription;
-    private letters: LetterService;
-  
-    constructor() { this.subscription = this.letters.currentMessage.subscribe(message => this.message = message); }
+    myTurn: boolean;
+    valueToEndGame: number;
+    maximumAllowedSkippedTurns: number = 3;
+    private messageSource = new BehaviorSubject('default message');
+    currentMessage = this.messageSource.asObservable();
 
-    sendMessage(message: string[]) {
-      this.subject.next(message);
-      this.message = message;
-  }
-  
-  
+    constructor(private letters: LetterService) {
+        this.subscription = this.letters.currentMessage.subscribe((message) => (this.message = message));
+    }
+
+    play() {
+      this.myTurn = parseInt(this.message) === 0;
+      if (this.myTurn === true) {
+      }
+    }
+
+    changeTurn(message: string) {
+      this.messageSource.next(message)
+    }
 }
