a8d708e120d5befae11bf3c5bf1e0e22681d525a
Artour <benevolenskiartour@gmail.com>
2021-09-18 16:37:58 -0400

    lint...

--- client/src/app/services/solo-player.service.ts
+++ client/src/app/services/solo-player.service.ts
@@ -47,7 +47,7 @@ export class SoloPlayerService {
         this.valueToEndGame++;
     }
 
-    exchangeLetters(){
+    exchangeLetters() {
         this.letters.exchangeLettersForPlayer();
     }
 }
