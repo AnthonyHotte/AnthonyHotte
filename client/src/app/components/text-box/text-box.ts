653fb6378f7e0a7619d45be2f1bb720684e5ed1e
Artour <benevolenskiartour@gmail.com>
2021-09-19 00:10:50 -0400

    Ajout imparfait de la commande de changement de tours

--- client/src/app/components/text-box/text-box.ts
+++ client/src/app/components/text-box/text-box.ts
@@ -1,20 +1,40 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { TextBox } from '@app/classes/text-box-behavior';
 import { ENTER_ASCII } from '@app/constants';
+import { GestionTimerTourService } from '@app/services/gestion-timer-tour.service';
+import { LetterService } from '@app/services/letter.service';
+import { SoloPlayerService } from '@app/services/solo-player.service';
+import { Subscription } from 'rxjs';
+
+
 
 @Component({
     selector: 'app-text-box',
     templateUrl: './text-box.html',
     styleUrls: ['./text-box.scss'],
 })
-export class TextBoxComponent {
+export class TextBoxComponent implements OnInit {
+    messagePlayer: string;
+    messageLetterService: string;
+    messageTimeMenager: string;
+    subscriptionPlayer: Subscription;
+    subscriptionLetterService: Subscription;
+    subscriptionTimeMenager: Subscription;
     word: string;
     array: string[];
     buttonCommandState: string;
     buttonMessageState: string;
     input: TextBox;
+    turn: number;
+    text: string;
 
-    constructor() {
+    constructor(
+        private soloPlayer: SoloPlayerService,
+        private letterService: LetterService,
+        private timeMenager: GestionTimerTourService,
+        private link: Router,
+    ) {
         this.word = '';
         this.array = [];
         this.buttonCommandState = 'ButtonCommandReleased';
@@ -33,11 +53,55 @@ export class TextBoxComponent {
         }
     }
 
+    ngOnInit() {
+        this.subscriptionPlayer = this.soloPlayer.currentMessage.subscribe((messagePlayer) => (this.messagePlayer = messagePlayer));
+        this.subscriptionLetterService = this.letterService.currentMessage.subscribe(
+            (messageLetterService) => (this.messageLetterService = messageLetterService),
+        );
+        this.subscriptionTimeMenager = this.timeMenager.currentMessage.subscribe(
+            (messageTimeMenager) => (this.messageTimeMenager = messageTimeMenager),
+        );
+        this.turn = this.timeMenager.turn;
+    }
+
     activateCommand() {
         this.input.activateCommandButton();
     }
 
     activateMessage() {
         this.input.activateMessageButton();
+    }
+
+    verifyCommandPassActiveTurn() {
+        const pass = '!passer';
+        this.turn = this.timeMenager.turn;
+        this.text = 'Tour de votre adversaire...';
+        if (this.buttonCommandState === 'ButtonCommandActivated' && this.turn === 0) {
+            if (this.array[0] === pass) {
+                if (this.soloPlayer.valueToEndGame < 2) {
+                    this.timeMenager.endTurn();
+                    this.turn = this.timeMenager.turn;
+                    this.soloPlayer.changeTurn(this.turn.toString());
+                    this.soloPlayer.incrementPassedTurns();
+                } else {
+                    this.finishCurrentGame();
+                }
+                this.text = 'Tour passé avec succès!';
+            } else {
+                this.text = 'La commande est invalide!';
+            }
+        }
+    }
+
+    finishCurrentGame() {
+        this.link.navigate(['home']);
+    }
+
+    getText() {
+        if (this.buttonCommandState === 'ButtonCommandActivated') {
+            const temp = this.text;
+            return temp;
+        }
+        return '';
     }
 }
