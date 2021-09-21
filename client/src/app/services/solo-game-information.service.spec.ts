6de424d523662df22b34316a3c325c4274d459a3
Anthony Hotte <anthonyhotte10@gmail.com>
2021-09-21 18:27:19 -0400

    A.H: gestion-timer-tour.service and solo-game-information.service all tested

--- client/src/app/services/solo-game-information.service.spec.ts
+++ client/src/app/services/solo-game-information.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { SoloGameInformationService } from './solo-game-information.service';
 
 describe('SoloGameInformationService', () => {
@@ -12,5 +11,29 @@ describe('SoloGameInformationService', () => {
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+    it('should send a message correctly', () => {
+        const arrMessage = ['Hello', 'World'];
+        service.sendMessage(arrMessage);
+        let index = 0;
+        for (const message of arrMessage) {
+            expect(service.message[index++]).toEqual(message);
+        }
+    });
+    it('send message should call next()', () => {
+        const nextSpy = spyOn(service.subject, 'next');
+        const arrMessage = ['Hello', 'World'];
+        service.sendMessage(arrMessage);
+        expect(nextSpy).toHaveBeenCalledWith(arrMessage);
+    });
+    it('clear message should call next()', () => {
+        const nextSpy = spyOn(service.subject, 'next');
+        service.clearMessages();
+        expect(nextSpy).toHaveBeenCalled();
+    });
+    it('get message should call asObservable()', () => {
+        const nextSpy = spyOn(service.subject, 'asObservable');
+        service.getMessage();
+        expect(nextSpy).toHaveBeenCalled();
     });
 });
