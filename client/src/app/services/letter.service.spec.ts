97a499f22f32af8e65f7810127a6ac3cf9372492
Anthony Hotte <anthonyhotte10@gmail.com>
2021-09-24 15:32:08 -0400

    A.H: small change

--- client/src/app/services/letter.service.spec.ts
+++ client/src/app/services/letter.service.spec.ts
@@ -16,4 +16,24 @@ describe('LetterService', () => {
     it('attribute allLetter should not be empty', () => {
         expect(service.allLetters.length).toBeGreaterThan(0);
     });
+    it('addLettersForPlayer should do nothing when pass 8', () => {
+        const letterToAdd = 8;
+        service.addLettersForPlayer(letterToAdd);
+        expect(service.currentLetterNumberForPlayer).toEqual(0);
+    });
+    it('addLettersForPlayer should add 7 letters when pass 7', () => {
+        const letterToAdd = 7;
+        service.addLettersForPlayer(letterToAdd);
+        expect(service.currentLetterNumberForPlayer).toEqual(letterToAdd);
+    });
+    it('addLettersForOpponent should do nothing when pass 8', () => {
+        const letterToAdd = 8;
+        service.addLettersForOpponent(letterToAdd);
+        expect(service.currentLetterNumberForPlayer).toEqual(0);
+    });
+    it('addLettersForOpponent should add 7 letters when pass 7', () => {
+        const letterToAdd = 7;
+        service.addLettersForOpponent(letterToAdd);
+        expect(service.currentLetterNumberForPlayer).toEqual(letterToAdd);
+    });
 });
