b7e489cc45a5e7cbf110e00fd7720b95542a8cc5
AnthonyHotte <78088719+AnthonyHotte@users.noreply.github.com>
2021-10-01 12:14:48 -0400

    A.H:Dead Code

--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { GameSelectionPageComponent } from '@app/pages/game-selection-page/game-selection-page.component';
-import { HighestScoresComponent } from '@app/pages/highest-scores/highest-scores.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 import { SoloGameInitiatorComponent } from '@app/pages/solo-game-initiator/solo-game-initiator.component';
@@ -12,7 +11,6 @@ const routes: Routes = [
     { path: 'home', component: MainPageComponent },
     { path: 'game', component: GamePageComponent },
     { path: 'material', component: MaterialPageComponent },
-    { path: 'scores', component: HighestScoresComponent },
     { path: 'selection', component: GameSelectionPageComponent },
     { path: 'soloInitiator', component: SoloGameInitiatorComponent },
     { path: '**', redirectTo: '/home' },
