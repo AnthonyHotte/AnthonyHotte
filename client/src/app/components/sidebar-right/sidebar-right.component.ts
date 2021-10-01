adfe98b9c096eb3c6ef585c212c5aae4f3153987
JJaillet <julien.jaillet@polymtl.ca>
2021-10-01 18:11:06 -0400

    fixing building error

--- client/src/app/components/sidebar-right/sidebar-right.component.ts
+++ client/src/app/components/sidebar-right/sidebar-right.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { PlayerLetterHand } from '@app/classes/player-letter-hand';
 import { TextBox } from '@app/classes/text-box-behavior';
-import { MessagePlayer } from '@app/message';
+// import { MessagePlayer } from '@app/message';
 import { GridService } from '@app/services/grid.service';
 import { LetterService } from '@app/services/letter.service';
 import { PlaceLettersService } from '@app/services/place-letters.service';
