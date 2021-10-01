6c58b22a4b60f697e4d78d7fef072aa4e200fa6c
AnthonyHotte <78088719+AnthonyHotte@users.noreply.github.com>
2021-10-01 18:45:44 -0400

    set up

--- client/src/app/services/solo-opponent2.service.spec.ts
+++ client/src/app/services/solo-opponent2.service.spec.ts
@@ -1,8 +1,29 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
+// import { GameStateService } from './game-state.service';
+// import { PlaceLettersService } from './place-letters.service';
 import { SoloOpponent2Service } from './solo-opponent2.service';
+import { WordValidationService } from './word-validation.service';
 
 describe('SoloOpponent2Service', () => {
     let service: SoloOpponent2Service;
+    let wordValidationServiceSpy: WordValidationService;
+    // let placeLettersServiceSpy: PlaceLettersService;
+    // let gameStateServiceSpy: GameStateService;
+    // let timerTurnManagerServiceSpy: TimerTurnManager;
+    // let letterServiceSpy: LetterService;
+
+    beforeEach(
+        waitForAsync(() => {
+            // letterServiceSpy = jasmine.createSpyObj('LetterService', ['getLettersForExchange']);
+            // timerTurnManagerServiceSpy = jasmine.createSpyObj('TimerTurnManagerService', ['initiateGame']);
+            // gameStateServiceSpy = jasmine.createSpyObj('GameStateService', ['placeLetter']);
+            // placeLettersServiceSpy = jasmine.createSpyObj('PlaceLettersService', ['checkInput']);
+            wordValidationServiceSpy = jasmine.createSpyObj('WordValidationService', ['isPartOfWordVertical']);
+            TestBed.configureTestingModule({
+                providers: [{ provide: WordValidationService, useValue: wordValidationServiceSpy }],
+            }).compileComponents();
+        }),
+    );
 
     beforeEach(() => {
         TestBed.configureTestingModule({});
