a62cb5d583e49170e7fc37139db6d1d6877e43f2
AnthonyHotte <78088719+AnthonyHotte@users.noreply.github.com>
2021-10-05 14:00:41 -0400

    fixing lettercomponent test

--- client/src/app/components/letters/letters.component.spec.ts
+++ client/src/app/components/letters/letters.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { PlayerLetterHand } from '@app/classes/player-letter-hand';
 import { LettersComponent } from '@app/components/letters/letters.component';
 import { LetterService } from '@app/services/letter.service';
 import { SoloOpponentService } from '@app/services/solo-opponent.service';
@@ -6,27 +9,30 @@ import { SoloOpponentService } from '@app/services/solo-opponent.service';
 describe('LettersComponent', () => {
     let component: LettersComponent;
     let fixture: ComponentFixture<LettersComponent>;
+    let routerSpy: jasmine.SpyObj<Router>;
     let letterServiceSpy: jasmine.SpyObj<LetterService>;
     let soloOpponentServiceSpy: jasmine.SpyObj<SoloOpponentService>;
 
     beforeEach(
         waitForAsync(() => {
+            routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+            letterServiceSpy = jasmine.createSpyObj('LetterService', ['reset']);
+            letterServiceSpy.players = [new PlayerLetterHand(), new PlayerLetterHand()];
+            soloOpponentServiceSpy = jasmine.createSpyObj('SoloOpponentService', ['reset']);
             TestBed.configureTestingModule({
                 declarations: [LettersComponent],
-                // providers: [
-                //     { provide: SoloPlayerService, useValue: soloPlayerServiceSpy },
-                //     { provide: SoloOpponentService, useValue: soloOpponentServiceSpy },
-                //     { provide: LetterService, useValue: letterServiceSpy },
-                // ],
+                providers: [
+                    { provide: Router, useValue: routerSpy },
+                    { provide: SoloOpponentService, useValue: soloOpponentServiceSpy },
+                    { provide: LetterService, useValue: letterServiceSpy },
+                ],
+                imports: [RouterTestingModule],
             }).compileComponents();
         }),
     );
 
     beforeEach(() => {
         fixture = TestBed.createComponent(LettersComponent);
-        letterServiceSpy = TestBed.inject(LetterService) as jasmine.SpyObj<LetterService>;
-        soloOpponentServiceSpy = TestBed.inject(SoloOpponentService) as jasmine.SpyObj<SoloOpponentService>;
-
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
@@ -34,12 +40,11 @@ describe('LettersComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
-    it('getNewLetters should call soloPlayerReset', () => {
-        const mySpy2 = spyOn(soloOpponentServiceSpy, 'reset');
-        component.currentLetterNumber = 0;
-        component.maxLettersInHand = 12;
+
+    it('getNewLetters get letter og player 0', () => {
+        component.letters = [];
+        letterServiceSpy.players[0].allLettersInHand = [{ letter: 'a', quantity: 1, point: 1 }];
         component.getNewLetters();
-        expect(mySpy2).toHaveBeenCalled();
-        expect(component.letters).toEqual(letterServiceSpy.players[0].allLettersInHand);
+        expect(component.letters.length).toEqual(1);
     });
 });
