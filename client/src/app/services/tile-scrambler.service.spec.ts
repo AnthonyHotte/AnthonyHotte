ef932b534bd1777dee547b7ad51aba9c47e6bedc
ismaelsaidi98 <78089589+ismaelsaidi98@users.noreply.github.com>
2021-10-05 18:56:07 -0400

    lint fixing

--- client/src/app/services/tile-scrambler.service.spec.ts
+++ client/src/app/services/tile-scrambler.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { TileScramblerService } from './tile-scrambler.service';
 
 describe('TileScramblerService', () => {
-  let service: TileScramblerService;
+    let service: TileScramblerService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TileScramblerService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(TileScramblerService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
