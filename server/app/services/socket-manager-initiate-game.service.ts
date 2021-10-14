ca2f0f425fdb6db82859b24ac1cb7175a61e8f17
Anthony Hotte <anthonyhotte10@gmail.com>
2021-10-14 10:48:49 -0400

    A.H: closing issue!

--- server/app/services/socket-manager-initiate-game.service.ts
+++ server/app/services/socket-manager-initiate-game.service.ts
@@ -7,10 +7,12 @@ export class SocketManager {
     private sio: io.Server;
     private rooms: Room[];
     private listRoomWaiting: Room[];
+    private indexNextRoom: number;
     constructor(server: http.Server) {
         this.sio = new io.Server(server, { cors: { origin: '*', methods: ['GET', 'POST'] } });
         this.rooms = [];
         this.listRoomWaiting = [];
+        this.indexNextRoom = 0;
         for (let i = 0; i < NUMBEROFROOMS; i++) {
             this.rooms.push(new Room('room number' + i, i));
         }
@@ -19,27 +21,36 @@ export class SocketManager {
     handleSockets(): void {
         this.sio.on('connection', (socket) => {
             socket.on('startingNewGameInfo', (message) => {
-                socket.join(this.rooms[0].roomName);
+                socket.join(this.rooms[this.indexNextRoom].roomName);
+                // set the room parameters
                 this.rooms[0].setStartingInfo(message.time, message.namePlayer, socket.id, message.bonusOn, message.mode);
                 // start game if in solo mode
                 if (message.mode === 'solo') {
                     socket.emit('startGame');
                 } else {
-                    this.listRoomWaiting.push(this.rooms[0]);
+                    // put the room in the waiting room list
+                    this.listRoomWaiting.push(this.rooms[this.indexNextRoom]);
                 }
+                // increment next room index but make sure it is not above 50
+                this.indexNextRoom = ++this.indexNextRoom % NUMBEROFROOMS;
             });
             socket.on('joinGame', (name) => {
-                socket.join(this.rooms[0].roomName);
-                this.rooms[0].playerNames.push(name);
-                this.rooms[0].socketsId.push(socket.id);
+                // join the oldest game in the waiting room
+                socket.join(this.rooms[this.listRoomWaiting[0].index].roomName);
+                // adding player name to the room
+                this.rooms[this.listRoomWaiting[0].index].playerNames.push(name);
+                // adding socket id to the room
+                this.rooms[this.listRoomWaiting[0].index].socketsId.push(socket.id);
+                // start game for everyone in the room
+                this.sio.to(this.rooms[this.listRoomWaiting[0].index].roomName).emit('startGame', this.rooms[this.indexNextRoom]);
+                // take of the room from waiting room
                 this.listRoomWaiting.splice(0, 1);
-                this.sio.to(this.rooms[0].roomName).emit('startGame', this.rooms[0]);
             });
             socket.on('disconnect', () => {
-                const index = this.rooms[0].socketsId.indexOf(socket.id);
+                const index = this.rooms[this.indexNextRoom].socketsId.indexOf(socket.id);
                 if (index !== ERRORCODE) {
-                    this.rooms[0].socketsId.splice(index, 1);
-                    this.rooms[0].playerNames.splice(index, 1);
+                    this.rooms[this.indexNextRoom].socketsId.splice(index, 1);
+                    this.rooms[this.indexNextRoom].playerNames.splice(index, 1);
                 }
             });
         });
