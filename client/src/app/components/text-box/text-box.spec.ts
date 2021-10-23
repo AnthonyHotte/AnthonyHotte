e1889a1d3fdf70328fa5d9c84ce780224a1589e8
Anthony Hotte <anthonyhotte10@gmail.com>
2021-10-23 16:43:05 -0400

    fixing test

--- client/src/app/components/text-box/text-box.spec.ts
+++ client/src/app/components/text-box/text-box.spec.ts
@@ -1,7 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { PlayerLetterHand } from '@app/classes/player-letter-hand';
 import { TextBox } from '@app/classes/text-box-behavior';
+import { MAXLETTERINHAND } from '@app/constants';
+import { LetterBankService } from '@app/services/letter-bank.service';
 import { LetterService } from '@app/services/letter.service';
 // import { TimerTurnManagerService } from '@app/services/timer-turn-manager.service';
 import { TextBoxComponent } from './text-box';
@@ -12,8 +15,20 @@ describe('TextBoxComponent', () => {
     let textBoxServiceSpy: jasmine.SpyObj<TextBox>;
     // let timeServiceSpy: jasmine.SpyObj<TimerTurnManagerService>;
     let letterServiceSpy: jasmine.SpyObj<LetterService>;
+    let letterBankServiceSpy: jasmine.SpyObj<LetterBankService>;
 
     beforeEach(async () => {
+        letterBankServiceSpy = jasmine.createSpyObj('LetterBankService', ['getLettersInBank']);
+        letterServiceSpy = jasmine.createSpyObj('LetterService', ['reset']);
+        const player1 = new PlayerLetterHand(letterBankServiceSpy);
+        for (let i = 0; i < MAXLETTERINHAND; i++) {
+            player1.allLettersInHand.push({ letter: 'a', quantity: 1, point: 1 });
+        }
+        const player2 = new PlayerLetterHand(letterBankServiceSpy);
+        for (let i = 0; i < MAXLETTERINHAND; i++) {
+            player2.allLettersInHand.push({ letter: 'a', quantity: 1, point: 1 });
+        }
+        letterServiceSpy.players = [player1, player2];
         textBoxServiceSpy = jasmine.createSpyObj('TextBox', [
             'activateCommandButton',
             'activateMessageButton',
@@ -27,13 +42,17 @@ describe('TextBoxComponent', () => {
         await TestBed.configureTestingModule({
             declarations: [TextBoxComponent],
             imports: [FormsModule, RouterTestingModule],
-            providers: [{ provide: TextBox, useValue: textBoxServiceSpy }],
+            providers: [
+                { provide: TextBox, useValue: textBoxServiceSpy },
+                { provide: LetterService, useValue: letterServiceSpy },
+            ],
         }).compileComponents();
     });
 
     beforeEach(() => {
         fixture = TestBed.createComponent(TextBoxComponent);
         component = fixture.componentInstance;
+        component.message = { message: '', sender: letterServiceSpy.players[0].name, role: 'Joueur' };
         fixture.detectChanges();
     });
 
