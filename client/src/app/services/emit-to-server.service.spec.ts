e721c9d0740764d8129a9e995fe2b63ce1727343
Anthony Hotte <anthonyhotte10@gmail.com>
2021-10-25 13:28:35 -0400

    test fixing

--- client/src/app/services/emit-to-server.service.spec.ts
+++ client/src/app/services/emit-to-server.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { EmitToServer } from '@app/services/emit-to-server.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('EmitToServer.TsService', () => {
     let service: EmitToServer;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            imports: [RouterTestingModule],
+        });
         service = TestBed.inject(EmitToServer);
     });
 
