4d33d452472659345a6ea3546a2f4ab7606c0aa7
ismaelsaidi98 <78089589+ismaelsaidi98@users.noreply.github.com>
2021-10-28 00:47:23 -0400

    Place letter click over

--- client/src/app/services/place-letter-click.service.ts
+++ client/src/app/services/place-letter-click.service.ts
@@ -33,19 +33,38 @@ export class PlaceLetterClickService {
             this.lastKeyPressed = letter;
             if (letter === 'Backspace' && this.wordPlacedWithClick.length !== 0) {
                 this.removeLetterWithBackspace();
-            } else if (this.letterService.players[0].handContainLetters(letter)) {
-                if (!this.isLetterAtEdge) {
-                    this.gridService.drawLetterwithpositionstring(letter, this.colomnNumber, this.row, 'red');
-                    this.lettersFromHand += letter;
-                    this.letterService.players[0].removeLettersWithoutReplacingThem(letter);
-                    this.wordPlacedWithClick += letter;
-                    this.handleRowAndColumnAfterLetter();
+            } else {
+                const normalizedLetter = letter.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+                const regexInput = /[a-zA-Z]/;
+                if (regexInput.exec(normalizedLetter) !== null && normalizedLetter.length === 1) {
+                    if (normalizedLetter.toLowerCase() !== normalizedLetter) {
+                        if (this.letterService.players[0].handContainLetters('*')) {
+                            this.addLetterOnBoard(normalizedLetter, true);
+                        }
+                    } else {
+                        if (this.letterService.players[0].handContainLetters(normalizedLetter)) {
+                            this.addLetterOnBoard(normalizedLetter, false);
+                        }
+                    }
                 }
-                if (!this.isLetterAtEdge) {
-                    this.gridService.drawarrow(this.orientation, this.row, this.colomnNumber);
-                }
-                // TODO delete last arrow after word is validated
             }
+        }
+    }
+    addLetterOnBoard(letter: string, isLetterCaps: boolean) {
+        if (!this.isLetterAtEdge) {
+            this.gridService.drawLetterwithpositionstring(letter.toLowerCase(), this.colomnNumber, this.row, 'red');
+            this.lettersFromHand += letter;
+            if (isLetterCaps) {
+                this.letterService.players[0].removeLettersWithoutReplacingThem('*');
+            } else {
+                this.letterService.players[0].removeLettersWithoutReplacingThem(letter);
+            }
+
+            this.wordPlacedWithClick += letter;
+            this.handleRowAndColumnAfterLetter();
+        }
+        if (!this.isLetterAtEdge) {
+            this.gridService.drawarrow(this.orientation, this.row, this.colomnNumber);
         }
     }
     caseSelected(xPos: number, yPos: number) {
@@ -142,7 +161,13 @@ export class PlaceLetterClickService {
         this.isLetterAtEdge = false;
         this.handleColumnAndRowAfterRemove();
         this.wordPlacedWithClick = this.wordPlacedWithClick.substr(0, this.wordPlacedWithClick.length - 1);
-        const tempLetter = LetterMap.letterMap.letterMap.get(this.lettersFromHand.charAt(this.lettersFromHand.length - 1)) as Letter;
+        const lastChar = this.lettersFromHand.charAt(this.lettersFromHand.length - 1);
+        let tempLetter: Letter;
+        if (lastChar !== lastChar.toLowerCase()) {
+            tempLetter = LetterMap.letterMap.letterMap.get('*') as Letter;
+        } else {
+            tempLetter = LetterMap.letterMap.letterMap.get(this.lettersFromHand.charAt(this.lettersFromHand.length - 1)) as Letter;
+        }
         this.letterService.players[0].allLettersInHand.push(tempLetter);
         this.lettersFromHand = this.lettersFromHand.substr(0, this.lettersFromHand.length - 1);
         this.gridService.drawtilebackground(this.colomnNumber, this.row);
