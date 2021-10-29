951b57ac45a0c0443d3b6d4b041bfb451b53f516
ismaelsaidi98 <78089589+ismaelsaidi98@users.noreply.github.com>
2021-10-29 12:01:33 -0400

    added and fixed test client side

--- client/src/app/components/place-word-with-click-gui/place-word-with-click-gui.component.spec.ts
+++ client/src/app/components/place-word-with-click-gui/place-word-with-click-gui.component.spec.ts
@@ -1,14 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TextBox } from '@app/classes/text-box-behavior';
+import { PlaceLetterClickService } from '@app/services/place-letter-click.service';
+import { PlaceLettersService } from '@app/services/place-letters.service';
+import { TimerTurnManagerService } from '@app/services/timer-turn-manager.service';
 
 import { PlaceWordWithClickGuiComponent } from './place-word-with-click-gui.component';
 
 describe('PlaceWordWithClickGuiComponent', () => {
     let component: PlaceWordWithClickGuiComponent;
     let fixture: ComponentFixture<PlaceWordWithClickGuiComponent>;
+    let textBoxSpy: jasmine.SpyObj<TextBox>;
+    let routerSpy: jasmine.SpyObj<Router>;
+    let placeLetterServiceSpy: jasmine.SpyObj<PlaceLettersService>;
+    let timeManagerSpy: jasmine.SpyObj<TimerTurnManagerService>;
+    let placeLetterClickServiceSpy: jasmine.SpyObj<PlaceLetterClickService>;
 
     beforeEach(async () => {
+        textBoxSpy = jasmine.createSpyObj('ClickManagementService', ['click']);
+        routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+        placeLetterServiceSpy = jasmine.createSpyObj('PlaceLettersService', ['getLettersForExchange', 'reset']);
+        placeLetterClickServiceSpy = jasmine.createSpyObj('PlaceLetterClickService', ['reset', 'caseSelected']);
+        timeManagerSpy = jasmine.createSpyObj('TimerTurnManagerService', ['endTurn', 'initiateGame']);
+
         await TestBed.configureTestingModule({
             declarations: [PlaceWordWithClickGuiComponent],
+            providers: [
+                { provide: TextBox, useValue: textBoxSpy },
+                { provide: Router, useValue: routerSpy },
+                { provide: PlaceLettersService, useValue: placeLetterServiceSpy },
+                { provide: TimerTurnManagerService, useValue: timeManagerSpy },
+                { provide: PlaceLetterClickService, useValue: placeLetterClickServiceSpy },
+            ],
+            imports: [RouterTestingModule],
         }).compileComponents();
     });
 
--- client/src/app/components/play-area/play-area.component.spec.ts
+++ client/src/app/components/play-area/play-area.component.spec.ts
@@ -1,15 +1,48 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TextBox } from '@app/classes/text-box-behavior';
 import { Vec2 } from '@app/classes/vec2';
 import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
+import { ClickManagementService } from '@app/services/click-management.service';
+import { GridService } from '@app/services/grid.service';
+import { LetterService } from '@app/services/letter.service';
+import { PlaceLetterClickService } from '@app/services/place-letter-click.service';
+import { PlaceLettersService } from '@app/services/place-letters.service';
 
 describe('PlayAreaComponent', () => {
     let component: PlayAreaComponent;
     let fixture: ComponentFixture<PlayAreaComponent>;
     let mouseEvent: MouseEvent;
+    let gridServiceSpy: jasmine.SpyObj<GridService>;
+    let letterServiceSpy: jasmine.SpyObj<LetterService>;
+    let placeLetterClickServiceSpy: jasmine.SpyObj<PlaceLetterClickService>;
+    let placeLetterServiceSpy: jasmine.SpyObj<PlaceLettersService>;
+    let textBoxSpy: jasmine.SpyObj<TextBox>;
+    let routerSpy: jasmine.SpyObj<Router>;
+    let clickManagerSpy: jasmine.SpyObj<ClickManagementService>;
 
     beforeEach(async () => {
+        gridServiceSpy = jasmine.createSpyObj('GridService', ['increasePoliceSize', 'drawGrid']);
+        placeLetterServiceSpy = jasmine.createSpyObj('PlaceLettersService', ['policeSizeChanged']);
+        placeLetterClickServiceSpy = jasmine.createSpyObj('PlaceLetterClickService', ['reset', 'caseSelected']);
+        routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+        textBoxSpy = jasmine.createSpyObj('TextBox', ['send', 'isCommand']);
+        letterServiceSpy = jasmine.createSpyObj('LetterService', ['getLettersForExchange', 'reset']);
+        clickManagerSpy = jasmine.createSpyObj('ClickManagementService', ['click', 'manageView']);
+
         await TestBed.configureTestingModule({
             declarations: [PlayAreaComponent],
+            providers: [
+                { provide: GridService, useValue: gridServiceSpy },
+                { provide: LetterService, useValue: letterServiceSpy },
+                { provide: PlaceLetterClickService, useValue: placeLetterClickServiceSpy },
+                { provide: PlaceLettersService, useValue: placeLetterServiceSpy },
+                { provide: TextBox, useValue: textBoxSpy },
+                { provide: ClickManagementService, useValue: clickManagerSpy },
+                { provide: Router, useValue: routerSpy },
+            ],
+            imports: [RouterTestingModule],
         }).compileComponents();
     });
 
