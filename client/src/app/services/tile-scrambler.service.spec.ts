4ce5fbb055fb3376453c7c8024c178bec87fa65b
ismaelsaidi98 <78089589+ismaelsaidi98@users.noreply.github.com>
2021-10-28 22:21:25 -0400

    test tile scrambler

--- client/src/app/services/tile-scrambler.service.spec.ts
+++ client/src/app/services/tile-scrambler.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { TileMap } from '@app/classes/grid-special-tile';
 
 import { TileScramblerService } from './tile-scrambler.service';
 
@@ -12,5 +13,39 @@ describe('TileScramblerService', () => {
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('scambleTiles should call getBonusTiles reassignNewPosition setNewMapValues', () => {
+        const spyBonus = spyOn(service, 'getBonusTiles');
+        const spyNewPosition = spyOn(service, 'reassignNewPosition');
+        const spyNewMap = spyOn(service, 'setNewMapValues');
+        service.scrambleTiles();
+        expect(spyBonus).toHaveBeenCalled();
+        expect(spyNewPosition).toHaveBeenCalled();
+        expect(spyNewMap).toHaveBeenCalled();
+    });
+
+    it('getBonusTiles should call forEach once', () => {
+        const spyforEach = spyOn(TileMap.gridMap.tileMap, 'forEach');
+        service.getBonusTiles();
+        expect(spyforEach).toHaveBeenCalled();
+    });
+
+    it('reassignNewPosition should call push at least 4 times', () => {
+        const spyPushDL = spyOn(service.newPositionDoubleLetter, 'push');
+        const spyPushDW = spyOn(service.newPositionDoubleWord, 'push');
+        const spyPushTL = spyOn(service.newPositionTripleLetter, 'push');
+        const spyPushTW = spyOn(service.newPositionTripleWord, 'push');
+        service.reassignNewPosition();
+        expect(spyPushDL).toHaveBeenCalled();
+        expect(spyPushDW).toHaveBeenCalled();
+        expect(spyPushTL).toHaveBeenCalled();
+        expect(spyPushTW).toHaveBeenCalled();
+    });
+
+    it('setNewMapValues should call set ', () => {
+        const spySet = spyOn(TileMap.gridMap.tileMap, 'set');
+        service.setNewMapValues();
+        expect(spySet).toHaveBeenCalled();
     });
 });
