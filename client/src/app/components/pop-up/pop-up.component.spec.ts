5dd24379e0147f84aca76d628f5d7b7dd3e3b606
ismaelsaidi98 <78089589+ismaelsaidi98@users.noreply.github.com>
2021-10-31 14:48:34 -0400

    pop up test done

--- client/src/app/components/pop-up/pop-up.component.spec.ts
+++ client/src/app/components/pop-up/pop-up.component.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { PopUpData } from '@app/classes/pop-up-data';
 
 import { PopUpComponent } from './pop-up.component';
 
-describe('PopUpComponent', () => {
+fdescribe('PopUpComponent', () => {
     let component: PopUpComponent;
     let fixture: ComponentFixture<PopUpComponent>;
+    let dialogRefSpy: jasmine.SpyObj<MatDialogRef<PopUpComponent>>;
+    let dataSpy: jasmine.SpyObj<PopUpData>;
 
     beforeEach(async () => {
+        dialogRefSpy = jasmine.createSpyObj(MatDialogRef, ['close']);
         await TestBed.configureTestingModule({
             declarations: [PopUpComponent],
+            providers: [
+                { provide: MatDialogRef, useValue: dialogRefSpy },
+                { provide: PopUpData, useValue: dataSpy },
+                { provide: MAT_DIALOG_DATA, useValue: {} },
+            ],
         }).compileComponents();
     });
 
@@ -20,5 +30,10 @@ describe('PopUpComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('onConfirm should call close', () => {
+        component.onConfirm();
+        expect(dialogRefSpy.close).toHaveBeenCalled();
     });
 });
