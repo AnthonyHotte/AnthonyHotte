819a193209cd6a1209c3a98bcb42b17ba7df4780
ismaelsaidi98 <78089589+ismaelsaidi98@users.noreply.github.com>
2021-11-01 19:59:23 -0400

    removed useless hostlistener

--- client/src/app/components/letters/letters.component.ts
+++ client/src/app/components/letters/letters.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as Constants from '@app/constants';
 import { Letter } from '@app/letter';
 import { LetterService } from '@app/services/letter.service';
@@ -16,11 +16,6 @@ export class LettersComponent implements OnInit {
     letterSize: number;
 
     constructor(private letterService: LetterService, private timerTurnManager: TimerTurnManagerService) {}
-    // call only at the beginning
-    @HostListener('keydown', ['$event'])
-    buttonDetect(event: KeyboardEvent) {
-        this.letterService.setIndexSelectedSwapping(event.key);
-    }
 
     getNewLetters(): void {
         this.letters = this.letterService.players[0].allLettersInHand;
