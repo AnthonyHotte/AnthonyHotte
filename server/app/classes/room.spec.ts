b456d2790c8fd80694b370dd913e2736bd0e6ab0
Anthony Hotte <anthonyhotte10@gmail.com>
2021-11-03 08:55:56 -0400

    fixing test server side

--- server/app/classes/room.spec.ts
+++ server/app/classes/room.spec.ts
@@ -13,7 +13,7 @@ describe('Room', () => {
 
     it('setStartingInfo should set the room', () => {
         const timeGame = 30;
-        room.setStartingInfo(timeGame, 'Antho', 'hdf6547');
+        room.setStartingInfo(timeGame, 'Antho', 'hdf6547', false, [{ letter: 'a', quantity: 1, point: 1 }], [{ letter: 'a', quantity: 1, point: 1 }]);
 
         expect(room.roomName).equal('room1');
         expect(room.playerNames[0]).equal('Antho');
@@ -28,5 +28,31 @@ describe('Room', () => {
         expect(room.bonusOn).equal(false);
         expect(room.playerNames[0]).equal('joueur1');
         expect(room.timePerTurn).equal(defaultTimePerTurn);
+    });
+    it('set room occupied', () => {
+        room.roomIsAvailable = true;
+        room.setRoomOccupied();
+        expect(room.roomIsAvailable).equal(false);
+    });
+    it('set room available', () => {
+        room.roomIsAvailable = false;
+        room.setRoomAvailable();
+        expect(room.roomIsAvailable).equal(true);
+    });
+    it('returnLettersInString ...', () => {
+        room.lettersCreator = [
+            { letter: 'a', quantity: 1, point: 1 },
+            { letter: 'e', quantity: 1, point: 1 },
+        ];
+        const res = room.returnLettersInString(true);
+        expect(res).equal('ae');
+    });
+    it('returnLettersInString ...', () => {
+        room.lettersJoiner = [
+            { letter: 'a', quantity: 1, point: 1 },
+            { letter: 'e', quantity: 1, point: 1 },
+        ];
+        const res = room.returnLettersInString(false);
+        expect(res).equal('ae');
     });
 });
