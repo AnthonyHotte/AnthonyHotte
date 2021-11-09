e6ceff9f9633813151b1dff5d0275ad69d3b8645
Anthony Hotte <anthonyhotte10@gmail.com>
2021-11-09 16:18:54 -0500

    fixing test

--- client/src/app/pages/admin-page/admin-page.component.spec.ts
+++ client/src/app/pages/admin-page/admin-page.component.spec.ts
@@ -1,14 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CommunicationService } from '@app/services/communication.service';
+import { Observable } from 'rxjs';
 
 import { AdminPageComponent } from './admin-page.component';
 
 fdescribe('AdminPageComponent', () => {
     let component: AdminPageComponent;
     let fixture: ComponentFixture<AdminPageComponent>;
+    let communicationServiceSpy: jasmine.SpyObj<CommunicationService>;
 
     beforeEach(async () => {
+        communicationServiceSpy = jasmine.createSpyObj('CommunicationService', [
+            'getDictionaryList',
+            'reinitialiseDictionary',
+            'sendDictionaryNameChanged',
+            'sendDeleteDictionary',
+            'getFullDictionary',
+        ]);
+        communicationServiceSpy.getDictionaryList.and.returnValue(new Observable());
         await TestBed.configureTestingModule({
             declarations: [AdminPageComponent],
+            providers: [{ provide: CommunicationService, useValue: communicationServiceSpy }],
         }).compileComponents();
     });
 
