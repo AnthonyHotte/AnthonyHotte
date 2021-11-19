482241bffa8c29580158af61f669fa7965646306
Anthony Hotte <anthonyhotte10@gmail.com>
2021-11-19 09:06:20 -0500

    finishing admin

--- client/src/app/services/dictionary.service.ts
+++ client/src/app/services/dictionary.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Dictionary } from '@app/classes/dictionary';
+import { BehaviorSubject } from 'rxjs';
 import { CommunicationService } from './communication.service';
 
 @Injectable({
@@ -9,11 +10,15 @@ export class DictionaryService {
     indexDictionary: number;
     dictionaryList: Dictionary[];
     isUploadComplete: boolean;
+    showUploadedInfo: boolean;
+    showButton: BehaviorSubject<boolean[]>;
     constructor(private communicationService: CommunicationService) {
         // default dictionary
         this.dictionaryList = [];
         this.indexDictionary = 0;
         this.isUploadComplete = false;
+        this.showUploadedInfo = false;
+        this.showButton = new BehaviorSubject<boolean[]>([true, false]);
     }
     getDictionary() {
         this.communicationService.getFullDictionary(this.indexDictionary).subscribe((res) => {
