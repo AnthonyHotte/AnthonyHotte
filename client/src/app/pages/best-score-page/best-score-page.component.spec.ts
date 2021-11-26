194d53d7a8ba5a5b7bb98836ee9480a0a77ff806
JJaillet <julien.jaillet@polymtl.ca>
2021-11-26 14:24:52 -0500

    fixing lint

--- client/src/app/pages/best-score-page/best-score-page.component.spec.ts
+++ client/src/app/pages/best-score-page/best-score-page.component.spec.ts
@@ -3,23 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BestScorePageComponent } from './best-score-page.component';
 
 describe('BestScorePageComponent', () => {
-  let component: BestScorePageComponent;
-  let fixture: ComponentFixture<BestScorePageComponent>;
+    let component: BestScorePageComponent;
+    let fixture: ComponentFixture<BestScorePageComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BestScorePageComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [BestScorePageComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BestScorePageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(BestScorePageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
