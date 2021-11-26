caf3b7087e8e51031967a60f7fca8c2e02b5c1e0
Anthony Hotte <anthonyhotte10@gmail.com>
2021-11-26 15:12:16 -0500

    main page component test

--- client/src/app/pages/main-page/main-page.component.spec.ts
+++ client/src/app/pages/main-page/main-page.component.spec.ts
@@ -2,7 +2,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
+import { BestScoreService } from '@app/services/best-score.service';
 import { CommunicationService } from '@app/services/communication.service';
+import { SocketService } from '@app/services/socket.service';
 import { of } from 'rxjs';
 import SpyObj = jasmine.SpyObj;
 
@@ -10,16 +12,24 @@ describe('MainPageComponent', () => {
     let component: MainPageComponent;
     let fixture: ComponentFixture<MainPageComponent>;
     let communicationServiceSpy: SpyObj<CommunicationService>;
+    let bestScoreServiceSpy: SpyObj<BestScoreService>;
+    let socketServiceSpy: SpyObj<SocketService>;
 
     beforeEach(async () => {
-        communicationServiceSpy = jasmine.createSpyObj('ExampleService', ['basicGet', 'basicPost']);
+        socketServiceSpy = jasmine.createSpyObj('SocketService', ['getMessageObservable']);
+        socketServiceSpy.is2990 = false;
+        bestScoreServiceSpy = jasmine.createSpyObj('BestScoreService', ['updateBestScore']);
+        communicationServiceSpy = jasmine.createSpyObj('CommunicationService', ['basicGet', 'basicPost', 'getBestScoreClassique']);
         communicationServiceSpy.basicGet.and.returnValue(of({ title: '', body: '' }));
         communicationServiceSpy.basicPost.and.returnValue(of());
 
         await TestBed.configureTestingModule({
             imports: [RouterTestingModule, HttpClientModule],
             declarations: [MainPageComponent],
-            providers: [{ provide: CommunicationService, useValue: communicationServiceSpy }],
+            providers: [
+                { provide: CommunicationService, useValue: communicationServiceSpy },
+                { provide: BestScoreService, useValue: bestScoreServiceSpy },
+            ],
         }).compileComponents();
     });
 
@@ -45,5 +55,13 @@ describe('MainPageComponent', () => {
     it('should call basicPost when calling sendTimeToServer', () => {
         component.sendTimeToServer();
         expect(communicationServiceSpy.basicPost).toHaveBeenCalled();
+    });
+    it('updateBestScore should call updateBestScore', () => {
+        component.updateBestScore();
+        expect(bestScoreServiceSpy.updateBestScore).toHaveBeenCalled();
+    });
+    it('setMode2990 should call updateBestScore', () => {
+        component.setMode2990(true);
+        expect(socketServiceSpy.is2990).toBe(true);
     });
 });
