0f4b985d50486293d9316fb08f0e7f27a4339457
ismaelsaidi98 <78089589+ismaelsaidi98@users.noreply.github.com>
2021-11-28 15:30:35 -0500

    fixed lint in sologame intiator

--- client/src/app/components/change-font-size/change-font-size.component.spec.ts
+++ client/src/app/components/change-font-size/change-font-size.component.spec.ts
@@ -3,23 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ChangeFontSizeComponent } from './change-font-size.component';
 
 describe('ChangeFontSizeComponent', () => {
-  let component: ChangeFontSizeComponent;
-  let fixture: ComponentFixture<ChangeFontSizeComponent>;
+    let component: ChangeFontSizeComponent;
+    let fixture: ComponentFixture<ChangeFontSizeComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ChangeFontSizeComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ChangeFontSizeComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ChangeFontSizeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ChangeFontSizeComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
