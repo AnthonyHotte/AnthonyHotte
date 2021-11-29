bd1177ea53278a307495365fe81ac6e903585c8f
JJaillet <julien.jaillet@polymtl.ca>
2021-11-28 22:37:24 -0500

    testing a deployment issue where the client would not connect to the server

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
     production: false,
-    serverUrl: 'http://localhost:3000/api',
+    // serverUrl: 'http://localhost:3000/api',
+    serverUrl: 'http://ec2-15-222-10-209.ca-central-1.compute.amazonaws.com:3000',
 };
 
 /*
