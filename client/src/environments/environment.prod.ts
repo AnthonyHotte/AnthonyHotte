7847a27bef1e44b81af19a254feb83d975389578
JJaillet <julien.jaillet@polymtl.ca>
2021-11-30 14:42:22 -0500

    forgot some changes

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://ec2-15-222-10-209.ca-central-1.compute.amazonaws.com:3000/api',
-    socketUrl: 'http://ec2-15-222-10-209.ca-central-1.compute.amazonaws.com:3000',
+    serverUrl: 'http://ec2-35-182-113-148.ca-central-1.compute.amazonaws.com:3000/api',
+    socketUrl: 'http://ec2-35-182-113-148.ca-central-1.compute.amazonaws.com:3000',
 };
