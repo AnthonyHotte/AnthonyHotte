d4bb7b735793d64a6ae04a39a84918207ab1f5c5
JJaillet <julien.jaillet@polymtl.ca>
2021-11-30 14:32:43 -0500

    changing environment for new aws instance

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -5,10 +5,10 @@
 export const environment = {
     production: false,
     // serverUrl: 'http://localhost:3000/api',
-    // serverUrl: 'http://localhost:3000/api',
-    // socketUrl: 'http://localhost:3000',
-    serverUrl: 'http://ec2-15-222-10-209.ca-central-1.compute.amazonaws.com:3000/api',
-    socketUrl: 'http://ec2-15-222-10-209.ca-central-1.compute.amazonaws.com:3000',
+    serverUrl: 'http://localhost:3000/api',
+    socketUrl: 'http://localhost:3000',
+    // serverUrl: 'http://ec2-15-222-10-209.ca-central-1.compute.amazonaws.com:3000/api',
+    // socketUrl: 'http://ec2-15-222-10-209.ca-central-1.compute.amazonaws.com:3000',
 };
 
 /*
