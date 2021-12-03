b200e814d4cbd99de64df53c377c4bf1a6ab4ce5
Anthony Hotte <anthonyhotte10@gmail.com>
2021-12-03 15:53:38 -0500

    test change font size conponent

--- client/src/app/components/change-font-size/change-font-size.component.spec.ts
+++ client/src/app/components/change-font-size/change-font-size.component.spec.ts
@@ -1,16 +1,26 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { GridService } from '@app/services/grid.service';
+import { PlaceLettersService } from '@app/services/place-letters.service';
 
 import { ChangeFontSizeComponent } from './change-font-size.component';
 
-describe('ChangeFontSizeComponent', () => {
+fdescribe('ChangeFontSizeComponent', () => {
     let component: ChangeFontSizeComponent;
     let fixture: ComponentFixture<ChangeFontSizeComponent>;
+    let gridServiceSpy: jasmine.SpyObj<GridService>;
+    let placeLettersServiceSpy: jasmine.SpyObj<PlaceLettersService>;
 
     beforeEach(async () => {
+        placeLettersServiceSpy = jasmine.createSpyObj('PlaceLettersService', ['policeSizeChanged']);
+        gridServiceSpy = jasmine.createSpyObj('GridService', ['increasePoliceSize', 'decreasePoliceSize']);
         await TestBed.configureTestingModule({
             declarations: [ChangeFontSizeComponent],
+            providers: [
+                { provide: GridService, useValue: gridServiceSpy },
+                { provide: PlaceLettersService, useValue: placeLettersServiceSpy },
+            ],
             imports: [RouterTestingModule, HttpClientTestingModule],
         }).compileComponents();
     });
@@ -23,5 +33,13 @@ describe('ChangeFontSizeComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+    it('increaseFontSize should call policeSizeChanged', () => {
+        component.increaseFontSize();
+        expect(placeLettersServiceSpy.policeSizeChanged).toHaveBeenCalled();
+    });
+    it('decreaseFontSize should call policeSizeChanged', () => {
+        component.decreaseFontSize();
+        expect(placeLettersServiceSpy.policeSizeChanged).toHaveBeenCalled();
     });
 });
